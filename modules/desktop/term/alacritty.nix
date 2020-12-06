e9504db836f2dcf66f9f635191ea7a53b30f4726
CaptainSpof <captain.spof@gmail.com>
2020-12-06 14:09:18 +0100

    alacritty: fix font
    - fix config after upgrade

--- modules/desktop/term/alacritty.nix
+++ modules/desktop/term/alacritty.nix
@@ -7,9 +7,7 @@ with lib;
 with lib.my;
 let cfg = config.modules.desktop.term.alacritty;
 in {
-  options.modules.desktop.term.alacritty = {
-    enable = mkBoolOpt false;
-  };
+  options.modules.desktop.term.alacritty = { enable = mkBoolOpt false; };
 
   config = mkIf cfg.enable {
     # xst-256color isn't supported over ssh, so revert to a known one
@@ -17,20 +15,24 @@ in {
       [ "$TERM" = xst-256color ] && export TERM=xterm-256color
     '';
 
-    user.packages = with pkgs; [
-      alacritty
-      # (makeDesktopItem {
-      #   name = "xst";
-      #   desktopName = "Suckless Terminal";
-      #   genericName = "Default terminal";
-      #   icon = "utilities-terminal";
-      #   exec = "${xst}/bin/xst";
-      #   categories = "Development;System;Utility";
-      # })
-    ];
+    user.packages = with pkgs;
+      [
+        alacritty
+        # (makeDesktopItem {
+        #   name = "xst";
+        #   desktopName = "Suckless Terminal";
+        #   genericName = "Default terminal";
+        #   icon = "utilities-terminal";
+        #   exec = "${xst}/bin/xst";
+        #   categories = "Development;System;Utility";
+        # })
+      ];
 
     home.configFile = {
-      "alacritty".source = "${configDir}/alacritty";
+      "alacritty" = {
+        source = "${configDir}/alacritty";
+        recursive = true;
+      };
     };
   };
 }
