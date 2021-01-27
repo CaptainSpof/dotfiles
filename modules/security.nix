ccea2c790d0fd50d5790311afb2f048560c7cbc4
CaptainSpof <captain.spof@gmail.com>
2021-01-27 20:34:45 +0100

    security: add pam kwallet service

--- modules/security.nix
+++ modules/security.nix
@@ -1,10 +1,16 @@
-{ ... }:
+{ config, lib, ... }:
 
+with lib;
 {
   ## System security tweaks
   boot.tmpOnTmpfs = true;
   security.hideProcessInformation = true;
   security.protectKernelImage = true;
+
+  security.pam.services.kwallet = mkIf (config.modules.desktop.plasma.enable) {
+    name = "kwallet";
+    enableKwallet = true;
+  };
 
   # Fix a security hole in place for backwards compatibility. See desc in
   # nixpkgs/nixos/modules/system/boot/loader/systemd-boot/systemd-boot.nix
