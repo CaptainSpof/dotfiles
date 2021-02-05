a36f411dc5865e98cc958d5ab07a51798d18cd37
CaptainSpof <captain.spof@gmail.com>
2021-02-05 13:36:50 +0100

    bump

--- modules/services/touchegg.nix
+++ modules/services/touchegg.nix
@@ -10,26 +10,36 @@ in {
 
   config = mkIf cfg.enable {
 
+    # FIXME: the derivation already provide this service file. I need to figure out how to enable it.
     systemd.services.touchegg = {
       enable = true;
-      description = "touchegg, libinput and stuff. The Daemon.";
+      description = "Touchégg. The daemon.";
       wantedBy = [ "multi-user.target" ];
       serviceConfig = {
         Type = "simple";
         Group = "input";
         Restart = "on-failure";
         RestartSec = 5;
+        # FIXME: how to link to a user derivation ?
+        # maybe, I should declare a touchegg variable with let and use that here ?
         ExecStart = "/usr/bin/touchegg --daemon";
       };
     };
 
-    systemd.user.services.touchegg = {
-      description = "touchegg, libinput and stuff. The Client.";
-      script = ''
-        touchegg
-      '';
+    # REVIEW: Don't need that, xdg/autostart does that for us.
+    # But I noticed touchegg segfaulting on some occasion.
+    # This service restart it when it happen.
+    systemd.user.services.touchegg-client = {
+      description = "Touchégg. The client.";
+
       wantedBy = [ "graphical-session.target" ];
       partOf = [ "graphical-session.target" ];
+
+      serviceConfig = {
+        Restart = "on-failure";
+        # FIXME: I'm ugly.
+        ExecStart = "/home/daf/.nix-profile/bin/touchegg";
+      };
     };
 
     home.configFile = {
