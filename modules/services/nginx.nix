10142749810c85f79ab2c75beb01cdf67eb155d1
CaptainSpof <captain.spof@gmail.com>
2021-02-06 18:03:09 +0100

    add dafpihole to the fleet

--- modules/services/nginx.nix
+++ modules/services/nginx.nix
@@ -36,69 +36,62 @@ in {
       virtualHosts = {
         "daftop" = {
           # locations."/adguard".proxyPass = "http://localhost:1337";
+          locations."/pihole".proxyPass = "http://dafpihole:80/admin";
           locations."/sync".proxyPass = "http://localhost:8384";
+        };
+        "pihole.daf" = {
+          locations."/".proxyPass = "http://dafpihole";
         };
         "sync.daf" = {
           locations."/".proxyPass = "http://localhost:8384";
 
           extraConfig = ''
-        proxy_set_header        Host localhost;
-      '';
+            proxy_set_header        Host localhost;
+          '';
         };
       };
-
-
-      # DNS proxy to AdGuard
-    #   appendConfig = ''
-    # stream {
-    #   server {
-    #     listen 53 udp;
-    #     proxy_pass localhost:1053;
-    #   }
-    # }
-    # '';
     };
 
     # AdguardHome container
     # TODO: Move in own module.
-    containers.adguard = {
-      autoStart = false;
+    # containers.adguard = {
+    #   autoStart = false;
 
-      config = { config, pkgs, ... }: {
+    #   config = { config, pkgs, ... }: {
 
-        environment.systemPackages = with pkgs; [
-          unstable.adguardhome
-        ];
+    #     environment.systemPackages = with pkgs; [
+    #       unstable.adguardhome
+    #     ];
 
-        systemd.services.adguardRunner = {
-          description = "AdGuard Home";
-          wantedBy = [ "multi-user.target" ];
-          after = [ "multi-user.target" ];
-          enable = true;
+    #     systemd.services.adguardRunner = {
+    #       description = "AdGuard Home";
+    #       wantedBy = [ "multi-user.target" ];
+    #       after = [ "multi-user.target" ];
+    #       enable = true;
 
-          serviceConfig = {
-            ExecStart = "${pkgs.unstable.adguardhome}/bin/adguardhome -w /var/lib/adguard -h localhost -p 1337";
-            Restart = "on-failure";
-            StartLimitIntervalSec = "15";
-            # Automatically creates and destroys user
-            isNormalUser = true;
-            # Automatically creates /var/lib/adguard
-            StateDirectory = "adguard";
-          };
-        };
-      };
-    };
+    #       serviceConfig = {
+    #         ExecStart = "${pkgs.unstable.adguardhome}/bin/adguardhome -w /var/lib/adguard -h localhost -p 1337";
+    #         Restart = "on-failure";
+    #         StartLimitIntervalSec = "15";
+    #         # Automatically creates and destroys user
+    #         isNormalUser = true;
+    #         # Automatically creates /var/lib/adguard
+    #         StateDirectory = "adguard";
+    #       };
+    #     };
+    #   };
+    # };
   };
 }
 
-# Helpful nginx snippets
-#
-# Set expires headers for static files and turn off logging.
-#   location ~* ^.+\.(js|css|swf|xml|txt|ogg|ogv|svg|svgz|eot|otf|woff|mp4|ttf|r ss|atom|jpg|jpeg|gif|png|ico|zip|tgz|gz|rar|bz2|doc|xls|exe|ppt|tar|mid|midi|wav |bmp|rtf)$ {
-#     access_log off; log_not_found off; expires 30d;
-#   }
-#
-# Deny all attempts to access PHP Files in the uploads directory
-#   location ~* /(?:uploads|files)/.*\.php$ {
-#     deny all;
-#   }
+  # Helpful nginx snippets
+  #
+  # Set expires headers for static files and turn off logging.
+  #   location ~* ^.+\.(js|css|swf|xml|txt|ogg|ogv|svg|svgz|eot|otf|woff|mp4|ttf|r ss|atom|jpg|jpeg|gif|png|ico|zip|tgz|gz|rar|bz2|doc|xls|exe|ppt|tar|mid|midi|wav |bmp|rtf)$ {
+  #     access_log off; log_not_found off; expires 30d;
+  #   }
+  #
+  # Deny all attempts to access PHP Files in the uploads directory
+  #   location ~* /(?:uploads|files)/.*\.php$ {
+  #     deny all;
+  #   }
