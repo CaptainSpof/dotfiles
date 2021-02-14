e60ceec88afeeb1b8803869e10c0468596420d88
CaptainSpof <captain.spof@gmail.com>
2021-02-14 12:58:13 +0100

    cleanup

--- config/plasma/autostart-scripts/ssh-add.sh
+++ config/plasma/autostart-scripts/ssh-add.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
+# TODO: make generic
 
 ssh-add -q ~/.ssh/gh-captainspof@daftop-nix.pem ~/.ssh/kering@daftop-nix.pem < /dev/null
--- hosts/personal.nix
+++ hosts/personal.nix
@@ -9,7 +9,7 @@ with lib;
          "192.168.0.31" = [ "dafbox" ];
          "192.168.0.33" = [ "dafpi" ];
          "192.168.0.35" = [ "dafpihole" ];
-         "127.0.0.1"    = [ "sync.daf" "adguard.daf" "pihole.daf" ];
+         "127.0.0.1"    = [ "sync.daf" "pihole.daf" ];
        };
        hosts = flatten (attrValues hostConfig);
        hostName = config.networking.hostName;
