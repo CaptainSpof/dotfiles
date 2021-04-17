28a61273e10193ffe39e90b6504f1ed8bb173d41
CaptainSpof <captain.spof@gmail.com>
2021-04-17 23:58:33 +0200

    bump

--- modules/desktop/apps/office/libreoffice.nix
+++ modules/desktop/apps/office/libreoffice.nix
@@ -11,7 +11,7 @@ in {
   config = mkIf cfg.enable {
     user.packages = with pkgs;
       (if config.modules.desktop.plasma.enable then [
-        libreoffice-qt
+        unstable.libreoffice-qt
       ] else [
         libreoffice-fresh
       ]);
